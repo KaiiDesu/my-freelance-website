a3c313c9a3441feda209f4889447f952bb3ece70
Klloyd Robert Advincula <exidragt@gmail.com>
2025-08-14 01:54:57 +0800

    Updates

--- _app/immutable/entry/start.BzLGjjJ9.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BgQR31kx.js";export{o as load_css,r as start};
---
+++ app_/immutable/chunks/BnHpXST3.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1uui10z?.base??"/my-freelance-website",e=globalThis.__sveltekit_1uui10z?.assets??s;export{e as a,s as b};
